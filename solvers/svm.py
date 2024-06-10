87117969014185e24a9e267868137b07a4443b9c
Thomas Moreau <thomas.moreau.2010@gmail.com>
2024-06-10 19:12:44 +0200

    Update svm.py

--- solvers/svm.py
+++ solvers/svm.py
@@ -6,7 +6,7 @@ from benchopt import BaseSolver, safe_import_context
 with safe_import_context() as import_ctx:
 
     # import your reusable functions here
-    from sklearn import svm
+    from sklearn.svm import SVC
 
 
 # The benchmark solvers must be named `Solver` and
@@ -18,16 +18,18 @@ class Solver(BaseSolver):
 
     # List of parameters for the solver. The benchmark will consider
     # the cross product for each key in the dictionary.
-    # All parameters 'p' defined here are available as 'self.p'.
+    # All parameters 'p' defined here are available as 'self.p'
+    # and are set to one value of the list.
     parameters = {
         'kernel': ['linear', 'poly', 'sigmoid'],
     }
 
     # List of packages needed to run the solver. See the corresponding
-    # section in objective.py
-    requirements = ['sklearn']
+    # section in objective.py. Here `scikit-learn` is already present
+    # so no need to add it again.
+    requirements = []
 
-    # Force solver to run only once if you don't want to record training steps
+    # Force the solver to run only once if you don't want to record training steps
     sampling_strategy = "run_once"
 
     def set_objective(self, X_train, y_train):
@@ -37,18 +39,19 @@ class Solver(BaseSolver):
         # passing the objective to the solver.
         # It is customizable for each benchmark.
         self.X_train, self.y_train = X_train, y_train
-        self.clf = svm.SVC(kernel=self.kernel)
+        self.clf = SVC(kernel=self.kernel)
 
-    def run(self, n_iter):
-        # This is the function that is called to fit the model.
-        # The param n_iter is defined if you change the sample strategy to other value than "run_once"
-        # https://benchopt.github.io/performance_curves.html
+    def run(self, _):
+        # This is the method that is called to fit the model.
+        # The input param is only defined if you change the sampling strategy
+        # to value different than "run_once".
+        # See https://benchopt.github.io/performance_curves.html
         self.clf.fit(self.X_train, self.y_train)
 
     def get_result(self):
         # Returns the model after fitting.
-        # The outputs of this function is a dictionary which defines the
-        # keyword arguments for `Objective.evaluate_result`
+        # The output of this function is a dictionary whose keys define the
+        # keyword arguments for `Objective.evaluate_result`.
         # This defines the benchmark's API for solvers' results.
-        # it is customizable for each benchmark.
+        # It is customizable for each benchmark.
         return dict(model=self.clf)
